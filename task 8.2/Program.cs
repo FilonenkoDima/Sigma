f2b88363ef18a48365a2d5a7802d5aa1e16fe208
FilonenkoDima <filonenkodmytrodm@gmail.com>
2022-06-27 18:12:17 +0300

    task 8.2

--- task 8.2/Program.cs
+++ task 8.2/Program.cs
@@ -5,8 +5,24 @@
         static void Main(string[] args)
         {
             FileProcessing fileProcessing = new FileProcessing();
-            DataProcessing dataProcessing = new DataProcessing(fileProcessing.ReadDataFileToList());
-            fileProcessing.WriteToResultFile(dataProcessing.GetStatistics());
+            string error = "";
+            try
+            {
+                DataProcessing dataProcessing = new DataProcessing(fileProcessing.ReadDataFileToList());
+                fileProcessing.WriteToResultFile(dataProcessing.GetStatistics());
+            }
+            catch (Exception ex)
+            {
+                error += ex.Message;
+            }
+            finally
+            {
+                if (error != "")
+                {
+                    fileProcessing.WriteLog(error);
+                    error = "";
+                }
+            }
         }
     }
 }
