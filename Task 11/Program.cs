2e0a1526f8eb3265e2238953780314aafbb08e48
FilonenkoDima <filonenkodmytrodm@gmail.com>
2022-07-19 15:36:48 +0300

    Task 11

---
+++ Task 11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace task112
+{
+
+    
+    class TestGenericList
+    {
+        static void Main()
+        {
+            GenericList<int> list = new GenericList<int>();
+
+            for (int x = 0; x < 10; x++)
+            {
+                list.AddHead(x);
+            }
+
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
